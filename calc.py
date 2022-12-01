c760f497bcb7a150892b038467163349b0ed8003
Roberto Rios <ccie.roberto.rios@gmail.com>
2022-12-01 12:10:58 -0800

    test

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-        ############# IP Calculator - Demo lab #############
+        ############# IP Calculator - Demo lab 123 #############
 >>>>>>> d187f835a045a8a4619ade6806d02e0f99b6ee36
 
 import random
