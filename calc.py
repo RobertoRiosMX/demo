111c0fab42fa06219ee9e73d97f07a240ce4f32c
Roberto Rios <ccie.roberto.rios@gmail.com>
2022-12-14 20:59:18 -0800

    Create calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-        ############# IP Calculator - demo #############
+        ############# IP Calculator - demo 123 #############
 >>>>>>> d187f835a045a8a4619ade6806d02e0f99b6ee36
 
 import random
